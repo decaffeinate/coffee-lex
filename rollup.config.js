37f54cdfb3502fb6594b38b0a3eb47ac653699fa
Brian Donovan <me@brian-donovan.com>
2016-06-16 16:23:59 -0700

    Include flow types in the package.
    Also updates the build system to use babelrc-rollup and the new
    `targets` configuration option of rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,21 @@
 import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
+
+var pkg = require('./package.json');
 
 export default {
   entry: 'src/index.js',
-  plugins: [babel({
-    babelrc: false,
-    presets: ['es2015-rollup'],
-    plugins: ['syntax-flow', 'transform-flow-strip-types', 'syntax-class-properties', 'transform-class-properties']
-  })]
+  plugins: [babel(babelrc())],
+  exports: 'named',
+  targets: [
+    {
+      format: 'es6',
+      dest: pkg['jsnext:main']
+    },
+    {
+      format: 'umd',
+      moduleName: 'coffeelex',
+      dest: pkg['main']
+    }
+  ]
 };
--- rollup.config.umd.js
+++
@@ -1,8 +0,0 @@
-import config from './rollup.config';
-
-config.dest = 'dist/coffee-lex.umd.js';
-config.format = 'umd';
-config.moduleName = 'coffeelex';
-config.outro = 'Object.defineProperty(exports, \'__esModule\', { value: true });';
-
-export default config;
