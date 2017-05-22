12c7d394eddbce77d9b57345375cdc8a65814f81
Alan Pierce <alangpierce@gmail.com>
2017-05-21 17:05:49 -0700

    fix: remove escaped newlines from triple-quoted strings (#100)
    Fixes https://github.com/decaffeinate/decaffeinate/issues/1025
    
    The rules for triple quoted and regular multiline strings are the same (it
    seems), so we can mostly just refactor it into a shared function used by both.

--- src/utils/calculateNormalStringPadding.ts
+++ src/utils/calculateNormalStringPadding.ts
@@ -3,6 +3,7 @@ import PaddingTracker from './PaddingTracker';
 
 import SourceLocation from '../SourceLocation';
 import BufferedStream from './BufferedStream';
+import isNewlineEscaped from "./isNewlineEscaped";
 
 /**
  * Compute the whitespace to remove in a multiline single or double quoted
@@ -73,25 +74,4 @@ export default function calculateNormalStringPadding(source: string, stream: Buf
     }
   }
   return paddingTracker.computeSourceLocations();
-}
-
-/**
- * A newline character is escaped if it's preceded by an odd number of
- * backslashes. Spaces are allowed between the backslashes and the newline.
- */
-function isNewlineEscaped(content: string, newlinePos: number): boolean {
-  let numSeenBackslashes = 0;
-  let prevPos = newlinePos - 1;
-  while (prevPos >= 0) {
-    let char = content[prevPos];
-    if (numSeenBackslashes === 0 && (char === ' ' || char === '\t')) {
-      prevPos--;
-    } else if (char === '\\') {
-      numSeenBackslashes++;
-      prevPos--;
-    } else {
-      break;
-    }
-  }
-  return numSeenBackslashes % 2 === 1;
 }
