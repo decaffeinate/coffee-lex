02df1e714c3493e8a28a7d265c8c86dfc326e3e8
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-07-09 08:25:18 -0700

    refactor: use constructor property shorthand

--- src/SourceLocation.ts
+++ src/SourceLocation.ts
@@ -4,11 +4,5 @@ import SourceType from './SourceType';
  * Represents a change in source code type at a particular index.
  */
 export default class SourceLocation {
-  readonly type: SourceType;
-  readonly index: number;
-
-  constructor(type: SourceType, index: number) {
-    this.type = type;
-    this.index = index;
-  }
+  constructor(readonly type: SourceType, readonly index: number) {}
 }
