1797d5526430ea3ba9e02f139b6f74354df77ddc
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-07-09 08:25:18 -0700

    refactor: use constructor property shorthand

--- src/SourceToken.ts
+++ src/SourceToken.ts
@@ -1,16 +1,15 @@
 import SourceType from './SourceType';
 
 export default class SourceToken {
-  readonly type: SourceType;
-  readonly start: number;
-  readonly end: number;
-
-  constructor(type: SourceType, start: number, end: number) {
+  constructor(
+    readonly type: SourceType,
+    readonly start: number,
+    readonly end: number
+  ) {
     if (start > end) {
-      throw new Error(`Token start may not be after end. Got ${type}, ${start}, ${end}`);
+      throw new Error(
+        `Token start may not be after end. Got ${type}, ${start}, ${end}`
+      );
     }
-    this.type = type;
-    this.start = start;
-    this.end = end;
   }
 }
