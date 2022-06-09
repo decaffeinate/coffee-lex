a20a754e50014d3ab93c2c2454814d80ed9cf540
zipang <christophe.Desguez@gmail.com>
2022-06-09 13:57:34 +0200

    fix: Error [ERR_PACKAGE_PATH_NOT_EXPORTED]: Package subpath './dist/Source***' is not defined by "exports"

--- tsup.config.js
+++ tsup.config.js
@@ -9,5 +9,50 @@ export default defineConfig([
   {
     entry: ['src/index.ts'],
     format: 'esm',
+  },
+  {
+    entry: ['src/SourceLocation.ts'],
+    dts: true,
+    format: 'cjs',
+  },
+  {
+    entry: ['src/SourceLocation.ts'],
+    format: 'esm',
+  },
+  {
+    entry: ['src/SourceToken.ts'],
+    dts: true,
+    format: 'cjs',
+  },
+  {
+    entry: ['src/SourceToken.ts'],
+    format: 'esm',
+  },
+  {
+    entry: ['src/SourceTokenList.ts'],
+    dts: true,
+    format: 'cjs',
+  },
+  {
+    entry: ['src/SourceTokenList.ts'],
+    format: 'esm',
+  },
+  {
+    entry: ['src/SourceTokenListIndex.ts'],
+    dts: true,
+    format: 'cjs',
+  },
+  {
+    entry: ['src/SourceTokenListIndex.ts'],
+    format: 'esm',
+  },
+  {
+    entry: ['src/SourceType.ts'],
+    dts: true,
+    format: 'cjs',
+  },
+  {
+    entry: ['src/SourceType.ts'],
+    format: 'esm',
   }
 ]);
